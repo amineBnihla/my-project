db501f822cf313457300f975dff749f5a3191958
amineBnihla <bnihla.10@gmail.com>
2021-12-29 11:32:40 +0100

    add changes to the project

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ let slides_fill = document.querySelectorAll('.skills .fill');
 //Setting How Much Time We Have For The Event
 let events = document.querySelector('.events');
  if(window.pageYOffset >= events.offsetTop){
-      let endDate = new Date("Dec 31,2021 16:00:59").getTime();
+      let endDate = new Date("Feb 31,2022 16:00:59").getTime();
         let interval = setInterval(()=>{
          let currentDate = new Date().getTime();
          let DateDiff = endDate - currentDate;
@@ -27,14 +27,21 @@ let events = document.querySelector('.events');
             clearInterval(interval);
         }
         let time = calculateTime(DateDiff);
- document.querySelector('.events .box.days h3').textContent = time.days;
- document.querySelector('.events .box.hours h3').textContent = time.hours;
- document.querySelector('.events .box.minutes h3').textContent = time.minutes;
- document.querySelector('.events .box.seconds h3').textContent = time.seconds;
+ document.querySelector('.events .box.days h3').textContent = time.days < 10 ? `0${time.days}`:time.days ;
+ document.querySelector('.events .box.hours h3').textContent = time.hours < 10 ? `0${time.hours}`:time.hours ;
+ document.querySelector('.events .box.minutes h3').textContent = time.minutes < 10 ? `0${time.minutes}`:time.minutes ;
+ document.querySelector('.events .box.seconds h3').textContent = time.seconds < 10 ? `0${time.seconds}`:time.seconds ;
         },1000); 
         }
 
+function calculateTime(Date){
+  let days = Math.floor(Date / (1000 * 60 *60 * 24)) ;
+        let hours = Math.floor((Date % (1000 * 60 * 60 * 24))/(1000 * 60 *60));
+        let minutes = Math.floor((Date % (1000 * 60 * 60 ))/(1000 * 60));
+        let seconds = Math.floor((Date % (1000 * 60 ))/(1000));
 
+        return {days,hours,minutes,seconds};
+}
         // Add Static Numbers 
 
 let stats = document.querySelector('.awesomeStats');
@@ -58,14 +65,7 @@ let stats = document.querySelector('.awesomeStats');
  }
 });
 
-function calculateTime(Date){
-  let days = Math.floor(Date / (1000 * 60 *60 * 24)) ;
-        let hours = Math.floor((Date % (1000 * 60 * 60 * 24))/(1000 * 60 *60));
-        let minutes = Math.floor((Date % (1000 * 60 * 60 ))/(1000 * 60));
-        let seconds = Math.floor((Date % (1000 * 60 ))/(1000));
 
-        return {days,hours,minutes,seconds};
-}
 
 
 
